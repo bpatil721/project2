fa85170b06150ad4dd22f37a1f13748008bdd83c
bhushan chaudhari <bhushanp721@gmail.com>
2025-07-03 16:52:33 +0530

    fix issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Category;
 class DatabaseSeeder extends Seeder
 {
     /**
